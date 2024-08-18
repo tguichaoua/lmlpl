38ef692a241e213a29da11dc81b03996b54d01a0
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2024-08-18 22:42:54 +0200

    🏷️ patch Object.entries typing

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+import { Entries } from 'type-fest';
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -7,6 +9,10 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+	}
+
+	interface ObjectConstructor {
+		entries<T extends object>(o: T): Entries<T>;
 	}
 }
 
