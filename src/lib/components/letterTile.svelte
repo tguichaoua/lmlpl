6d158e3a293f6c1071d1f3647abbd4fbbb5ef457
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2024-08-20 12:09:10 +0200

    💄 LetterTile

---
+++ src/lib/components/letterTile.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let letter: string;
+</script>
+
+<div
+	class="aspect-[2/3] w-[50px] select-none rounded-sm border-2 border-black text-center font-mono text-6xl"
+>
+	{letter}
+</div>
